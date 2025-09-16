8932884529f92f202945bbfd567e9be75d7d74ab
apgstocks <apgzip@gmail.com>
2025-09-16 17:51:31 +0530

    Commiting logger and customer exception

---
+++ exception/custom_exception.py
@@ -0,0 +1,27 @@
+import traceback,sys
+from logger.custom_logger import CustomLogger
+logger=CustomLogger().get_Logger(__file__)
+class DocumentPortalException(Exception):
+    
+    def __init__(self, error_message,error_details:sys):
+
+        _,_,exc_tb=error_details.exc_info()
+        self.file_name=exc_tb.tb_frame.f_code.co_filename
+        self.lineno=exc_tb.tb_lineno
+        self.error_message=str(error_message)
+        self.traceback_str=''.join(traceback.format_exception(*error_details.exc_info()))
+
+    def __str__(self):
+        return f"""
+        Error in [{self.file_name}] at line [{self.lineno}]
+        Error Message: {self.error_message}
+        Traceback: {self.traceback_str}
+        """
+
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        app_raise= DocumentPortalException(e,sys)
+        logger.error(DocumentPortalException(e, sys))  
+        raise app_raise 
--- logger/custom_logger.py
+++ logger/custom_logger.py
@@ -1,1 +1,26 @@
+import logging,os
+from datetime import datetime
+class CustomLogger:
+    def __init__(self,log_dir="logs"):
+        self.log_dirs=os.path.join(os.getcwd(),log_dir)
+        os.makedirs(self.log_dirs,exist_ok=True)
+        #creation of log file
+
+        log_file=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+        LOG_FILE_PATH=os.path.join(self.log_dirs,log_file)
+
+        #logger config
+        logging.basicConfig(
+            filename=LOG_FILE_PATH,
+            format='[%(asctime)s] %(levelname)s %(name)s (line:%(lineno)d) - %(message)s',
+            level=logging.INFO
+        )
+    def get_Logger(self,name=__file__):
+        return logging.getLogger(os.path.basename(name))
+    
+if __name__=="__main__":
+    logger=CustomLogger()
+    logger=logger.get_Logger(__file__)
+    logger.info('Custom Logger Initialised')
+
 
